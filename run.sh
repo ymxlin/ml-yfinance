10d448b1aafe76cb44778bd5609ddaba163025eb
Alvin Lin <lym@duck.com>
2026-03-14 21:03:51 +0000

    make fail2ban really works

--- run.sh
+++ run.sh
@@ -6,9 +6,11 @@ SW_LOG=$1
 
 # not picky, any char will do
 if [ -z "$SW_LOG" ]; then
+    # semi-production
     uvicorn main:app --host 0.0.0.0 --port 8000 --no-server-header >> /home/azureuser/app/logs/access.log 2>&1
 else
-    uvicorn main:app --host 0.0.0.0 --port 8000 --no-server-header
+    # debugging
+    uvicorn main:app --host 0.0.0.0 --port 8000 --no-server-header --reload
 fi
 
 #uvicorn main:app --host 0.0.0.0 --port 8000 --no-server-header >> /var/log/uvicorn/access.log 2>&1
