7feae363fbf7a22a36a49e8fba6e3b2d2478790b
Ubuntu <azureuser@alvinvmca.4n41f04qr0tevluzd5crgkyrvg.ux.internal.cloudapp.net>
2026-03-14 03:50:03 +0000

    new structure

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ SW_LOG=$1
 
 # not picky, any char will do
 if [ -z "$SW_LOG" ]; then
-    uvicorn main:app --host 0.0.0.0 --port 8000 --no-server-header >> /var/log/uvicorn/access.log 2>&1
+    uvicorn main:app --host 0.0.0.0 --port 8000 --no-server-header >> /home/azureuser/app/logs/access.log 2>&1
 else
     uvicorn main:app --host 0.0.0.0 --port 8000 --no-server-header
 fi
