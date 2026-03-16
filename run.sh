5528d1915d9b6e12d9603d78a26b030f46cc67b1
Alvin Lin <lym@duck.com>
2026-03-16 02:22:20 +0000

    enhance log format

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@ SW_LOG=$1
 # not picky, any char will do
 if [ -z "$SW_LOG" ]; then
     # semi-production
-    uvicorn main:app --host 0.0.0.0 --port 8000 --no-server-header >> /home/azureuser/app/logs/access.log 2>&1
+    uvicorn main:app --host 0.0.0.0 --port 8000 --no-server-header --log-config /home/azureuser/app/config/uvicorn/logging.yaml >> /home/azureuser/app/logs/access.log 2>&1
 else
     # debugging
-    uvicorn main:app --host 0.0.0.0 --port 8000 --no-server-header --reload
+    uvicorn main:app --host 0.0.0.0 --port 8000 --no-server-header --log-config '/home/azureuser/app/config/uvicorn/logging.yaml' --reload
 fi
 
 #uvicorn main:app --host 0.0.0.0 --port 8000 --no-server-header >> /var/log/uvicorn/access.log 2>&1
